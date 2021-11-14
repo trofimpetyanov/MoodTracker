aa63a3a43d1fe70e29b53b327fdf1b73106495ff
Trofim Petyanov <trofimpetyanov@icloud.com>
2021-11-14 18:46:45 +0400

    Initial Commit

---
+++ MoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodTracker
+//
+//  Created by Trofim Petyanov on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
